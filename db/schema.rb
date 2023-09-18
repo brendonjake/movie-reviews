403a520608d94bb1936eb2e6190fdd61704a70f2
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-18 11:44:59 +0800

    creating model for movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_17_140900) do
+  create_table "movies", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.decimal "total_gross", precision: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
