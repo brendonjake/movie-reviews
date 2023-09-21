baaabd219ac026fed7cd4c40354132959eec27b0
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-21 20:24:39 +0800

    adding flash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully updated!"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -29,7 +29,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully created!"
     else
       render :new, status: :unprocessable_entity
     end
@@ -38,7 +38,8 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url, status: :see_other
+    redirect_to movies_url, status: :see_other,
+                alert: "Movie successfully deleted!"
   end
 
 
