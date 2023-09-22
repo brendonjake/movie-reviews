a1b6a75ef03e915565407ad3fb33411234e8dc77
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-22 13:55:53 +0800

    creating user account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_121550) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_054428) do
   create_table "movies", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_121550) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "reviews", "movies"
