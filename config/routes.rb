7f19300b9b08a1969e922358a3bbf7a4147e0451
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-22 14:17:57 +0800

    creating user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-
+  get "signup" => "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
