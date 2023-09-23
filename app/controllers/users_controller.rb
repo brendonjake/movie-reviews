76a6f6388668f9cb63a091143eb5492d1cd218b6
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-23 11:35:01 +0800

    creating edit user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
     params.require(:user).
       permit(:name, :email, :password, :password_confirmation)
   end
-
 end
