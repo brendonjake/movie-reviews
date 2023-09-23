ee28167fc73056f75e04bf95190e7344c74021ea
LAPTOP-CJ75UCV4\Billionaire <macapaz.b1998@gmail.com>
2023-09-23 18:52:22 +0800

    creating sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :users
   get "signup" => "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
   # Defines the root path route ("/")
   root "movies#index"
 
